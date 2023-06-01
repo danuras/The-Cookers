b261b711264d0212aec1ba636e937dc2e6ed5e1f
Darussalaam Nur Rasyidu <darussalaamnurrasyidu@gmail.com>
2023-06-01 11:28:23 +0700

    penyesuaian route dasar

--- tests/Feature/DashboardIndexTest.php
+++ tests/Feature/DashboardIndexTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,8 +12,16 @@ class DashboardIndexTest extends TestCase
     /**
      * @test
      */
-    public function test_index(): void
+    public function test_home(): void
     {
+        // Membuat user baru untuk dihapus
+        $user = User::factory()->create();
+
+        // Menjalankan HTTP POST request ke route 'login' untuk mengotentikasi pengguna
+        $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'password', // Ganti dengan password pengguna yang valid
+        ]);
         // Mengirimkan permintaan HTTP GET ke rute 'dashboard.index'
         $response = $this->get('/');
 
@@ -20,7 +29,7 @@ class DashboardIndexTest extends TestCase
         $response->assertStatus(200);
 
         // Memastikan tampilan 'dashboard' digunakan
-        $response->assertViewIs('dashboard');
+        $response->assertViewIs('home');
 
         // Memastikan data 'f_recipes' dan 'n_recipes' tersedia di tampilan
         $response->assertViewHas(['f_recipes', 'n_recipes']);
@@ -30,12 +39,21 @@ class DashboardIndexTest extends TestCase
 
         // Memastikan data 'n_recipes' berisi instance dari LengthAwarePaginator
         $this->assertInstanceOf(\Illuminate\Pagination\LengthAwarePaginator::class, $response->viewData('n_recipes'));
+    }
+    /**
+     * @test
+     */
+    public function test_dashboard(): void
+    {
 
+        // Mengirimkan permintaan HTTP GET ke rute 'dashboard.index'
+        $response = $this->get('/');
 
+        // Memastikan respons berhasil (kode status 200)
+        $response->assertStatus(200);
 
-        /* // Memastikan data 'n_recipes' berisi instance dari Paginator
-        $response->assertViewHas('n_recipes', function ($recipes) {
-            return $recipes instanceof \Illuminate\Pagination\Paginator;
-        }); */
+        // Memastikan tampilan 'dashboard' digunakan
+        $response->assertViewIs('dashboard');
+
     }
 }
