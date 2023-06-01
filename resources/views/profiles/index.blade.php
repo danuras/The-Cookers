582c50def868bc4939b257ba6325b162731c5a31
Prennk <email.affanardana@gmail.com>
2023-06-01 15:17:38 +0700

    masukkin profil ke laravel

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,25 +4,22 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Responsive Profile Page</title>
-
-        <link rel="icon" type="image/png" href="{{asset('assets/cookers.png')}}">
-        <linkrel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+        <title>Profile My Receipt</title>
+        <!-- Font Awesome -->
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        />
+        <!-- CSS -->
         <link rel="stylesheet" href="css/style-profil.css" />
     </head>
     <body>
         <div class="header__wrapper">
-            <header>
-                <button class="btn-back">
-                    <a href="/">
-                        Kembali
-                    </a>
-                </button>
-            </header>
+            <header></header>
             <div class="cols__container">
                 <div class="left__col">
                     <div class="img__container">
-                        <img src={{ asset('images/img-profil/user.jpeg') }} alt="Anna Smith" />
+                        <img src="{{ asset('images/img-profil/user.jpeg') }}" alt="Anna Smith" />
                         <span></span>
                     </div>
                     <h2>Shani Indira</h2>
@@ -31,7 +28,6 @@
 
                     <ul class="about">
                         <li><span>25</span>Saved</li>
-                        <!-- <li><span>3</span></li> -->
                         <li><span>3</span>Receipt</li>
                     </ul>
 
@@ -42,77 +38,108 @@
                             <li><i class="fab fa-twitter"></i></li>
                             <i class="fab fa-pinterest"></i>
                             <i class="fab fa-facebook"></i>
-                            <i class="fab fa-dribbble"></i>
+                            <i class="fab fa-instagram"></i>
                         </ul>
                     </div>
                 </div>
                 <div class="right__col">
                     <nav>
                         <ul>
-                            <li><a href="">saved</a></li>
-                            <li><a href="">receipt</a></li>
-                            <!-- <li><a href="">groups</a></li>
-                            <li><a href="">about</a></li> -->
+                            <li><a href="">home</a></li>
+                            <li>
+                                <a
+                                    href="file:///C:/Users/Asus/Downloads/The-Cookers/resources/views/profil/profil/index.html"
+                                    >favorite</a
+                                >
+                            </li>
+                            <li>
+                                <a
+                                    href="file:///C:/Users/Asus/Downloads/The-Cookers/resources/views/profil/profil_myreceipt/index.html"
+                                    >my receipt</a
+                                >
+                            </li>
                         </ul>
                         <button>
                             <a href="{{ route('profiles.edit',$profiles->id) }}">
-                                Pengaturan Akun
+                                Account Setting
                             </a>
                         </button>
                     </nav>
 
-                    <div class="saved">
-                        <img
-                            src={{asset('images/img-profil/donat.jpg')}}
-                            alt="saved"
-                        />
-                        <img
-                            src={{asset('images/img-profil/ayam-bakar.jpg')}}
-                            alt="saved"
-                        />
-                        <img
-                            src={{asset('images/img-profil/brownies.jpg')}}
-                            alt="saved"
-                        />
-                        <img
-                            src={{asset('images/img-profil/burger.jpg')}}
-                            alt="saved"
-                        />
-                        <img
-                            src={{asset('images/img-profil/ikan-bumbu.jpg')}}
-                            alt="saved"
-                        />
-                        <img
-                            src={{asset('images/img-profil/kue-lumpur.jpg')}}
-                            alt="saved"
-                        />
+                    <div class="line1">
+                        <div class="saved">
+                            <img
+                                src="{{asset('images/img-profil/ayam-goreng.jpg')}}"
+                                alt="resep"
+                            />
+                            <img
+                                src="{{asset('images/img-profil/nasi-goreng.jpg')}}"
+                                alt="resep"
+                            />
+                            <img
+                                src="{{asset('images/img-profil/salad.jpg')}}"
+                                alt="resep"
+                            />
+                        </div>
+
+                        <div class="button1">
+                            <button>
+                                Ayam Goreng By Dinda
+                                <!-- <li>
+                                    <i class="fa-thin fa-bookmark"></i>
+                                </li> -->
+                            </button>
+                            <button>
+                                Nasi Goreng by Fatya
+                                <!-- <li>
+                                    <i class="fa-thin fa-bookmark"></i>
+                                </li> -->
+                            </button>
+                            <button>
+                                Salad Sayur by Salsa
+                                <!-- <li>
+                                    <i class="fa-thin fa-bookmark"></i>
+                                </li> -->
+                            </button>
+                        </div>
                     </div>
 
-                    <div class="resep">
-                        <img
-                            src="{{asset('images/img-profil/ayam-goreng.jpg')}}"
-                            alt="resep"
-                        />
-                        <img
-                            src={{asset('images/img-profil/nasi-goreng.jpg')}}
-                            alt="resep"
-                        />
-                        <img
-                            src={{asset('images/img-profil/salad.jpg')}}
-                            alt="resep"
-                        />
-                        <img
-                            src={{asset('images/img-profil/salmon.jpg')}}
-                            alt="resep"
-                        />
-                        <img
-                            src={{asset('images/img-profil/spageti.jpg')}}
-                            alt="resep"
-                        />
-                        <img
-                            src={{asset('images/img-profil/tumis-tahu.jpg')}}
-                            alt="resep"
-                        />
+                    <div class="line2">
+                        <div class="saved2">
+                            <img
+                                src="{{asset('images/img-profil/salmon.jpg')}}"
+                                alt="resep"
+                            />
+                            <img
+                                src="{{asset('images/img-profil/spageti.jpg')}}"
+                                alt="resep"
+                            />
+                            <img
+                                src="{{asset('images/img-profil/tumis-tahu.jpg')}}"
+                                alt="resep"
+                            />
+                        </div>
+
+                        <div class="button2">
+                            <button>
+                                Salmon By Kelvin
+                                <!-- <li>
+                                    <i class="fa-thin fa-bookmark"></i>
+                                </li> -->
+                            </button>
+                            <button>
+                                Spaghetti by Liliana
+                                <!-- <li>
+                                    <i class="fa-thin fa-bookmark"></i>
+                                </li> -->
+                            </button>
+                            <button>
+                                Tumis Tahu by Maria
+                                <!-- <li>
+                                    <i class="fa-thin fa-bookmark"></i>
+                                </li> -->
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
