91c7a45ffbe45ceba76efc04028bc131b8bd1f33
Darussalaam Nur Rasyidu <darussalaamnurrasyidu@gmail.com>
2023-06-15 10:28:01 +0700

    integrasi home

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     /**
-     * Memanggil halaman dashboard dan menampilkan data resep yang terpopuler dan terbaru dengan paginate bernilai 25, sehingga data yang ditampilkan akan
+     * Memanggil halaman dashboard dan menampilkan data resep yang terpopuler dan terbaru dengan paginate bernilai 4, sehingga data yang ditampilkan akan
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View 
      */
     public function index()
@@ -21,11 +21,11 @@ class DashboardController extends Controller
             $data['f_recipes'] = Recipe::select('id', 'image_url', 'name')
                 ->withCount('favorites')
                 ->orderByDesc('favorites_count')
-                ->paginate(25, ['*'], 'f_recipes');
+                ->limit(4)->get();
             // Mengambil data resep dengan mengurutkannya berdasarkan tanggal dibuatnya
             $data['n_recipes'] = Recipe::select('id', 'image_url', 'name')
                 ->orderByDesc('created_at')
-                ->paginate(25, ['*'], 'n_recipes');
+                ->limit(4)->get();
             // Mengirim data resep ke tampilan "dashboard" dan mengembalikan tampilan tersebut
             return view('home', $data);
         } else {
