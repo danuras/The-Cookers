483e1fc2980a242f3d995d149d69b2fc3f6e662c
anistyaroyidaa <anistya.88@students.amikom.ac.id>
2023-06-18 21:02:46 +0700

    menambahkan halaman edit resep kamu #3 dan #4

--- resources/views/editresep-upload/editresep-upload.js
+++ resources/views/editresep-upload/editresep-upload.js
@@ -35,6 +35,30 @@ document.getElementById("uploadForm").addEventListener("submit", function (e) {
     fileInput.value = "";
 });
 
+// var uploadForm = document.getElementById("uploadForm");
+// var photoInput = document.getElementById("photoInput");
+// var imageContainer1 = document.getElementById("imageContainer1");
+// var imageContainer2 = document.getElementById("imageContainer2");
+
+// uploadForm.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     var file = photoInput.files[0];
+//     var fileReader = new FileReader();
+
+//     fileReader.onload = function (event) {
+//         var image1 = document.createElement("img");
+//         image1.src = event.target.result;
+//         imageContainer1.appendChild(image1);
+
+//         var image2 = document.createElement("img");
+//         image2.src = event.target.result;
+//         imageContainer2.appendChild(image2);
+//     };
+
+//     fileReader.readAsDataURL(file);
+// });
+
 function goToPage(pageId) {
     var pages = document.getElementsByClassName("page");
     for (var i = 0; i < pages.length; i++) {
@@ -45,32 +69,86 @@ function goToPage(pageId) {
 }
 
 function displayResult() {
-    var name = document.getElementById("nameInput").value;
-    var email = document.getElementById("emailInput").value;
+    var nameInput = document.getElementById("nameInput").value;
+    var descriptionInput = document.getElementById("paragraphInput").value;
+    var cookingTimeInput = document.getElementById("numberInput").value;
+    var servingsInput = document.getElementById("servingInput").value;
 
     var resultContainer = document.getElementById("resultContainer");
-    resultContainer.innerHTML =
-        "<p>Nama: " + name + "</p><p>Email: " + email + "</p>";
+    resultContainer.innerHTML = `
+            <p><strong>Nama Resep:</strong> ${nameInput}</p>
+            <p><strong>Deskripsi Resep:</strong> ${descriptionInput}</p>
+            <p><strong>Waktu Memasak:</strong> ${cookingTimeInput} menit</p>
+            <p><strong>Jumlah Porsi:</strong> ${servingsInput}</p>
+        `;
 }
 
-function displayResult() {
-    var number = document.getElementById("numberInput").value;
-    var link = document.getElementById("linkInput").value;
+function tampilkanBahan() {
+    var ingredientsInput = document.getElementById("ingredientsInput").value;
+    var bahanList = document.getElementById("bahanList");
 
-    var resultContainer = document.getElementById("resultContainer");
-    resultContainer.innerHTML =
-        "<p>Angka: " +
-        number +
-        "</p><p>Tautan: <a href='" +
-        link +
-        "'>" +
-        link +
-        "</a></p>";
+    // Menghapus bahan-bahan sebelumnya
+    while (bahanList.firstChild) {
+        bahanList.removeChild(bahanList.firstChild);
+    }
+
+    // Memisahkan bahan-bahan menjadi array
+    var bahanArray = ingredientsInput.split("\n");
+
+    // Menambahkan bahan-bahan ke dalam daftar
+    bahanArray.forEach(function (bahan) {
+        var bahanItem = document.createElement("li");
+        bahanItem.textContent = bahan;
+        bahanList.appendChild(bahanItem);
+    });
 }
 
-function displayResult() {
-    var paragraph = document.getElementById("paragraphInput").value;
+function tampilkanLangkah() {
+    var stepsInput = document.getElementById("stepsInput").value;
+    var langkahList = document.getElementById("langkahList");
 
-    var resultContainer = document.getElementById("resultContainer");
-    resultContainer.innerHTML = "<p>" + paragraph + "</p>";
+    // Menghapus langkah-langkah sebelumnya
+    while (langkahList.firstChild) {
+        langkahList.removeChild(langkahList.firstChild);
+    }
+
+    // Memisahkan langkah-langkah menjadi array
+    var langkahArray = stepsInput.split("\n");
+
+    // Menambahkan langkah-langkah ke dalam daftar
+    langkahArray.forEach(function (langkah) {
+        var langkahItem = document.createElement("li");
+        langkahItem.textContent = langkah;
+        langkahList.appendChild(langkahItem);
+    });
+}
+
+function tampilkanVideo() {
+    var linkInput = document.getElementById("linkInput").value;
+    var videoContainer = document.getElementById("videoContainer");
+    var videoId = getYoutubeVideoId(linkInput);
+
+    if (videoId) {
+        videoContainer.innerHTML =
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/' +
+            videoId +
+            '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
+            '<img src="https://img.youtube.com/vi/' +
+            videoId +
+            '/maxresdefault.jpg" alt="Video Preview" width="560" height="315" />';
+    } else {
+        videoContainer.innerHTML = "Link Video YouTube tidak valid";
+    }
+}
+
+function getYoutubeVideoId(url) {
+    var regExp =
+        /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|\?v=)([^#&?]*).*/;
+    var match = url.match(regExp);
+
+    if (match && match[2].length === 11) {
+        return match[2];
+    } else {
+        return null;
+    }
 }
