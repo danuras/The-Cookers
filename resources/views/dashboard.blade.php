48bb8956e289b6409df1e1183f89b29b1462e71f
Prennk <email.affanardana@gmail.com>
2023-06-26 07:12:55 +0700

    finishing landing page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,16 +32,13 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item pe-3">
-                        <a class="nav-link active" href="#">Beranda</a>
+                        <a class="nav-link disabled" href="#">Beranda</a>
                     </li>
                     <li class="nav-item pe-3">
-                        <a class="nav-link active" href="#">Tentang Kami</a>
+                        <a class="nav-link disabled" href="#">Tentang Kami</a>
                     </li>
                     <li class="nav-item pe-3">
-                        <a class="nav-link active" href="#">Resep</a>
-                    </li>
-                    <li class="nav-item pe-3">
-                        <a class="nav-link active" href="#">Kontak</a>
+                        <a class="nav-link disabled" href="#">Kontak</a>
                     </li>
                     <li class="nav-item pe-3">
                         <button class="btn-auth">
